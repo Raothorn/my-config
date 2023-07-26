7570b79c3d05b7c45fccf9bda265ecc8f5dcfd04
Gaston Gilbert <gastongator@gmail.com>
2023-07-26 17:16:45 -0500

    init commit

---
+++ copy_config.sh
@@ -0,0 +1,7 @@
+mkdir -p .config/Code/User
+cp -r ~/.config/Code/User/* .config/Code/User/
+cp -r ~/.config/i3 .config/
+cp -r ~/.config/polybar .config/
+cp ~/.spacemacs .spacemacs
+cp ~/.Xresources .Xresources
+
